c9a3cde2086fb61ef4354a073e81fa6d14372962
zeref <abhijeetsingh173+vscode@gmail.com>
2025-07-26 22:49:51 +0530

    user

--- backend/Actions/Card.js
+++ backend/Actions/Card.js
@@ -1,14 +1,34 @@
-const {fileModel} = require("../models/user"); 
+const { fileModel, userModel } = require("../models/user");
 
 
-const Card = async (req,res)=>{
-    try{
-        const {userId} = req.body;
-        const data = await fileModel.find({id:userId});
-        res.status(200).json({data});
-    }catch(err){
+const Card = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const data = await fileModel.find({ id: userId });
+        res.status(200).json({ data });
+    } catch (err) {
         console.log(err);
     }
 }
+const UserD = async (req, res) => {
+    try {
+        const { userId } = req.body;
 
-module.exports = Card;
+        if (!userId) {
+            return res.status(400).json({ error: 'User ID is required.' });
+        }
+
+        const data = await userModel.findOne({ userId });
+        console.log(data)
+        if (!data) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+
+        res.status(200).json({ data });
+    } catch (err) {
+        console.error('Error fetching user:', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+module.exports = { Card, UserD };
